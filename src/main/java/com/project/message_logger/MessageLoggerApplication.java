4a4ed9a8a96b0ed686c0858ba18d9e56f57902f1
Haingo <haingo.razafindranto@gmail.com>
2025-05-11 18:33:23 -0400

    init project

---
+++ src/main/java/com/project/message_logger/MessageLoggerApplication.java
@@ -0,0 +1,12 @@
+package com.project.message_logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/message_logger/MessageLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.message_logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
